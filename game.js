5edd31d57a5ad4d38240839bf4b3f9f0b4f04395
mano-pihema <mano-pihema-laptop@LAPTOP-MNKDSN6R.localdomain>
2022-06-03 10:26:18 +1200

    added random func to utils

--- game.js
+++ game.js
@@ -1,12 +1,2 @@
 
 
-const arr = heroData.heros
-
-function getRandomHero(arr){
-let rand = Math.floor((Math.random)*arr.length)
-return rand[i]
-}
-
-module.exports ={
-  getRandomHero
-}
---
+++ utils.js
@@ -0,0 +1,10 @@
+const arr = heroData.heros
+
+function getRandomHero(arr){
+let rand = Math.floor(Math.random()*arr.length)
+return rand[i]
+}
+
+module.exports ={
+  getRandomHero
+}
