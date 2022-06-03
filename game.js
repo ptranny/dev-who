b88d35649009463e86f19c0e2cd6ee446fd36cac
Phuong Tran <ptranny@gmail.com>
2022-06-03 12:52:58 +1200

    Resolve game.js conflict

--- game.js
+++ game.js
@@ -1,78 +1,65 @@
-<<<<<<< HEAD
 // get characters
 
 //import random character func
 //  global character random character
-
-=======
 // read characters
 // make JSON.parse('characters)
 
->>>>>>> e1056124d78ed16aeeca24f5c2c9ccd25fe89049
 // generate the game board first
 // pull out all the td cells
 // put an image
 // put an image in each cell
 // attach onclick and ondoubleclick listeners
-<<<<<<< HEAD
 
 //  making handler / listen for onclick event of character guess
 
 //create replay button
 
-
 //  making handler / listen for single click event to toggle pictures
-
+//  making handler /listen for onclick event of next hint
 
 const characters = require('./characters.json')
 
 const getRandomHero = require(getRandomHero)
 
+const RANDOMHERO = getRandomHero(characters['characters'])
 
-const RANDOMHERO = getRandomHero(characters["characters"])
+function hideCharacter(e) {
+  //return element clicked
+  //find the class to change using id from target
+  //toggle is hidden class
 
-function hideCharacter(e){
-//return element clicked
-//find the class to change using id from target
-//toggle is hidden class
+  let elementId = e.target.id
 
-let elementId= e.target.id
+  let clickedCell = document.getElementById(elementId)
 
-let clickedCell = document.getElementById(elementId)
+  clickedCell.classList.toggle('isHidden')
+}
 
-clickedCell.classList.toggle('isHidden')
-
-} 
-
-//  making handler /listen for onclick event of next hint
 let HINTINDEX = 0
-function nextHint(){
+function nextHint() {
   //increment hint index
   //create var called current hint
   //update innerhtml with current hint
 
   HINTINDEX++
-  const hintArray = ['age','weapon']
-  let currentHint = `Hint: The ${hintArray[HINTINDEX]} is ${RANDOMHERO[hintArray[HINTINDEX]]}`
+  const hintArray = ['age', 'weapon']
+  let currentHint = `Hint: The ${hintArray[HINTINDEX]} is ${
+    RANDOMHERO[hintArray[HINTINDEX]]
+  }`
   document.getElementById('hint').innerHTML = currentHint
 }
 
-function doubleClick(e){
- //get id of cell clicked 
-//if id  == same td of random character
+function doubleClick(e) {
+  //get id of cell clicked
+  //if id  == same td of random character
 
-//then rediret to result partial
-//change innerHTML of id=result
+  //then rediret to result partial
+  //change innerHTML of id=result
 
-
-if(e.target.id ==RANDOMHERO.id){
-
+  if (e.target.id == RANDOMHERO.id) {
+  }
 }
-
-}
-
-
-
 
 function start() {
   bindEventListeners(document.getElementById('td'))
@@ -80,32 +67,10 @@ function start() {
 
 function bindEventListeners(cells) {
   for (var i = 0; i < cells.length; i++) {
-      // BIND YOUR EVENT LISTENERS HERE
-      cells[i].addEventListener('click', hideCharacter)
-      cells[i].addEventListener('dblclick', guessCharacter)
-          // create an image tag
-      cells[i].children[0].src = characters[i+1].image
+    // BIND YOUR EVENT LISTENERS HERE
+    cells[i].addEventListener('click', hideCharacter)
+    cells[i].addEventListener('dblclick', guessCharacter)
+    // create an image tag
+    cells[i].children[0].src = characters[i + 1].image
   }
-}
-=======
->>>>>>> e1056124d78ed16aeeca24f5c2c9ccd25fe89049
-
-//  making handler / listen for onclick event of character guess
-
-//  making handler /listen for onclick event of next hint
-
-//  making handler / listen for single click event to toggle pictures
-
-function start() {
-    bindEventListeners(document.getElementsByTagName('td'))
-}
-
-function bindEventListeners(cells) {
-    for (var i = 0; i < cells.length; i++) {
-        // BIND YOUR EVENT LISTENERS HERE
-        cells[i].addEventListener('click', hideCharacter)
-        cells[i].addEventListener('dblclick', guessCharacter)
-            // create an image tag
-        cells[i].children[0].src = characters[i].image
-    }
 }
