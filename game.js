63b8cfc8ff87b2cea48671a1fc33297f7f0a12cb
ljsimpkin@gmail.com <sandbox1@Sands-MacBook-Pro.local>
2022-06-03 14:19:09 +1200

    add hint generator

--- game.js
+++ game.js
@@ -26,66 +26,72 @@ const dataPath = '/characters.json'
 start(dataPath)
 
 async function start(dataPath) {
-  let response = await fetch(dataPath)
-  let data = await response.text()
-  let characters = JSON.parse(data).characters
+    let response = await fetch(dataPath)
+    let data = await response.text()
+    let characters = JSON.parse(data).characters
 
-  bindEventListeners(document.getElementsByTagName('td'))
+    bindEventListeners(document.getElementsByTagName('td'))
 
-  function bindEventListeners(cells) {
-    for (let i = 0; i < cells.length; i++) {
-      console.log(cells[i])
-      // BIND YOUR EVENT LISTENERS HERE
-      cells[i].addEventListener('click', hideCharacter)
-      cells[i].addEventListener('dblclick', guessCharacter)
-      // create an image tag
-      cells[i].children[0].src = characters[i].image
+    function bindEventListeners(cells) {
+        for (let i = 0; i < cells.length; i++) {
+            // BIND YOUR EVENT LISTENERS HERE
+            cells[i].addEventListener('click', hideCharacter)
+            cells[i].addEventListener('dblclick', guessCharacter)
+                // create an image tag
+            cells[i].children[0].src = characters[i].image
+        }
     }
-  }
 
-  const RANDOMHERO = getRandomHero(characters['characters'])
+    const RANDOMHERO = getRandomHero(characters)
 
-  function hideCharacter(e) {
-    //return element clicked
-    //find the class to change using id from target
-    //toggle is hidden class
+    function hideCharacter(e) {
+        //return element clicked
+        //find the class to change using id from target
+        //toggle is hidden class
 
-    let elementId = e.target.id
+        let elementId = e.target.id
 
-    let clickedCell = document.getElementById(elementId)
+        let clickedCell = document.getElementById(elementId)
 
-    clickedCell.classList.toggle('isHidden')
-  }
+        clickedCell.classList.toggle('isHidden')
+    }
 
-  let HINTINDEX = 0
-  function nextHint() {
-    //increment hint index
-    //create var called current hint
-    //update innerhtml with current hint
+    let HINTINDEX = 0
 
-    HINTINDEX++
-    const hintArray = ['age', 'weapon']
-    let currentHint = `Hint: The ${hintArray[HINTINDEX]} is ${
+    function nextHint() {
+        //increment hint index
+        //create var called current hint
+        //update innerhtml with current hint
+
+        HINTINDEX++
+        const hintArray = ['age', 'weapon']
+        let currentHint = `Hint: The ${hintArray[HINTINDEX]} is ${
       RANDOMHERO[hintArray[HINTINDEX]]
     }`
-    document.getElementById('hint').innerHTML = currentHint
-  }
-
-  function guessCharacter(e) {
-    //get id of cell clicked
-    //if id  == same td of random character
-
-    //then rediret to result partial
-    //change innerHTML of id=result
-
-    if (e.target.id == RANDOMHERO.id) {
+        document.getElementById('hint').innerHTML = currentHint
     }
-  }
+
+    let nextHintButton = document.getElementById('nextHint')
+    nextHintButton.addEventListener('click', nextHint)
+
+    function guessCharacter(e) {
+        console.log('inside')
+            //get id of cell clicked
+            //if id  == same td of random character
+            //then rediret to result partial
+            //change innerHTML of id=result
+        const cellClicked = e.target.id
+        if (e.target.id == RANDOMHERO.id) {
+            alert('Congrats you won')
+        } else {
+            alert('Ohh noo you lost! Better luck next time :)')
+        }
+    }
 }
 
 // UTILS
 
 function getRandomHero(arr) {
-  let rand = Math.floor(Math.random() * arr.length)
-  return arr[rand]
+    let rand = Math.floor(Math.random() * arr.length)
+    return arr[rand]
 }
